ecf7bf0d87eeeb5cef67997b95fbbe2cb9fb7fd2
Afoxgolf <austinwfox11@gmail.com>
2025-08-11 12:04:28 -0500

    added demo website example

---
+++ demo_website/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import gsap from 'gsap';
+import Navbar from './components/Navbar';
+import Hero from './components/hero';
+import Cocktails from './components/Cocktails';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <main>
+        <Navbar />
+        <Hero />
+        <Cocktails />
+    </main>
+  )
+}
+
+export default App
---
+++ demo_website/src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about'>
+        <div className='mb-16 md:px-0 px-5'>
+            <div className='content'>
+                <div className='md:col-span-8'>
+                <p className='badge'>Best Cocktails</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
